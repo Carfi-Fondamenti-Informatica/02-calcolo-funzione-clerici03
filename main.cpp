c1bcf9a6037045a31156527612b8e8344908553a
clerici03 <115361946+clerici03@users.noreply.github.com>
2022-11-15 09:34:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,12 +7,15 @@ int main (){
     float y=0;
     float z=0;
     cin >>a >>b >>x >>y;
-    if ( x<0 && y>0 ){
-        z=a*x-b*y;
-    } else if (x>=0 && y<=0){
-        z=a*(x*x)-b*y;
-    }else{
-        z=a*x-b*(y*y);
+    if  (x<0 && y>0) {
+        z=(a*x)-(b*y);
+    } 
+    else if (x>=0 && y<=0) {
+        z=(a*x*x)-(b*y);
+    }
+    else{
+        z=(a*x)-(b*y*y);
     }
     cout<< z <<endl;
+    return 0;
 }
