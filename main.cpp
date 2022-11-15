39db680074262115df77c9c1eb0aefcb668bf1fe
clerici03 <115361946+clerici03@users.noreply.github.com>
2022-11-15 09:29:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-   return 0;
+int main (){
+    float a=0;
+    float b=0;
+    float x=0;
+    float y=0;
+    float z=0;
+    cin >>a >>b >>x >>y;
+    if ( x<0 && y>0 ){
+        z=a*x-b*y;
+    } else if (x>=0 && y<=0){
+        z=a*(x*x)-b*y;
+    }else{
+        z=a*x-b*(y*y);
+    }
+    cout<< z <<endl;
 }
